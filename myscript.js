0149a5ee67b2d6ce4e32707c73a00b285fa23b2a
Alex Ye <ayxworks@gmail.com>
2017-09-30 13:39:48 +0200

    nav bar that follows
    try 1

--- myscript.js
+++ myscript.js
@@ -6,3 +6,19 @@ function myFunction() {
         x.className = "topnav";
     }
 }
+
+$(document).ready(function() {
+  
+  $(window).scroll(function () {
+      //if you hard code, then use console
+      //.log to determine when you want the 
+      //nav bar to stick.  
+      //console.log($(window).scrollTop())
+    if ($(window).scrollTop() > 1000) {
+      $('#nav_bar').addClass('navbar-fixed');
+    }
+    if ($(window).scrollTop() < 1001) {
+      $('#nav_bar').removeClass('navbar-fixed');
+    }
+  });
+});
