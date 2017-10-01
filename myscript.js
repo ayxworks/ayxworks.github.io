9b925c44f3a065f75abedd00d8b358f8425896c4
Alex Ye <ayxworks@gmail.com>
2017-10-01 12:40:06 +0200

    start of the slideshow
    same height

--- myscript.js
+++ myscript.js
@@ -16,10 +16,10 @@ $(document).ready(function() {
       //.log to determine when you want the 
       //nav bar to stick.  
       //console.log($(window).scrollTop())
-    if ($(window).scrollTop() > 500) {
+    if ($(window).scrollTop() > 450) {
       $('#nav_bar').addClass('navbar-fixed');
     }
-    if ($(window).scrollTop() < 501) {
+    if ($(window).scrollTop() < 451) {
       $('#nav_bar').removeClass('navbar-fixed');
     }
   });
